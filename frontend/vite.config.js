ecfc5b8b47b63d25487273b495ecf1af08c6ecef
neo s <neos@neos-MacBook-Air.local>
2024-08-30 18:52:03 +0300

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
         '/api':{
-          target:'http://localhost:3000',
+          target:'https://retrogames-e0ob.onrender.com',
           changeOrigin: true,
           secure: false,
           // rewrite: (path) => path.replace(/^\/api/, ''),
