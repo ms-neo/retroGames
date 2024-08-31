0ddac50e81cf2c09bb4f8b049e032c7814225c45
neo s <neos@neos-MacBook-Air.local>
2024-08-31 14:26:42 +0300

    1

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,12 @@ const connectDB = async () =>{
  
     try {
         console.log('conn',process.env.DB_URI)
-       await mongoose.connect(process.env.DB_URI)
+       await mongoose.connect(`mongodb+srv://${process.env.DB_URI}@db.a1thqsb.mongodb.net/retro-games?retryWrites=true&w=majority&appName=db
+       production`,{
+        useUnifiedTopology: true,
+
+        useNewUrlParser: true,
+       })
        console.log('connected to mongodb')
     } catch (err) {
         console.log(err)
--- backend/config/dist/db.dev.js
+++ backend/config/dist/db.dev.js
@@ -10,7 +10,10 @@ var connectDB = function connectDB() {
           _context.prev = 0;
           console.log('conn', process.env.DB_URI);
           _context.next = 4;
-          return regeneratorRuntime.awrap(mongoose.connect(process.env.DB_URI));
+          return regeneratorRuntime.awrap(mongoose.connect("mongodb+srv://".concat(process.env.DB_URI, "@db.a1thqsb.mongodb.net/retro-games?retryWrites=true&w=majority&appName=db\n       production"), {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+          }));
 
         case 4:
           console.log('connected to mongodb');
