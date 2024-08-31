cf92a507d07cefb1793c38091a9866bd3ad100c6
neo s <neos@neos-MacBook-Air.local>
2024-08-31 14:43:34 +0300

    2

--- frontend/src/components/image/Image.jsx
+++ frontend/src/components/image/Image.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Image = ({src,...rest}) => {
+  console.log(src,'src')
     //this code to display the game in dev env or prodc env
-    src = src && src.includes('https://retrogames-e0ob.onrender.com/')
-    ? src
+    src = src && src.includes('https://')
+    ? `https://retrogames-e0ob.onrender.com/${src}`
     : `http://localhost:3000/${src}`
   return (
     <img {...rest} src={src} alt={'games'}/>
