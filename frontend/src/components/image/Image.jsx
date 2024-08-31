006b4cb8712a0ad369ebedcee44445433a2339da
neo s <neos@neos-MacBook-Air.local>
2024-08-31 16:23:56 +0300

    img url

--- frontend/src/components/image/Image.jsx
+++ frontend/src/components/image/Image.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Image = ({src,...rest}) => {
 
   // const location = useLocation
-  console.log(window.location,'src')
+  console.log(window.location.href,'src')
     //this code to display the game in dev env or prodc env
-    src = src && window.location === 'https://retrogames-e0ob.onrender.com'
+    src = src && window.location.href === 'https://retrogames-e0ob.onrender.com'
     ? `https://retrogames-e0ob.onrender.com/${src}`
     : `http://localhost:3000/${src}`
   return (
